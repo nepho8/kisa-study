c9a7eae13705bd0e627a17a662ac6a6e4a08a2a4
K.KH <nepho@blocko.io>
2016-06-09 14:47:47 +0900

    study-3

--- client/main.js
+++ client/main.js
@@ -23,6 +23,9 @@ Template.hello.helpers({
 Template.hello.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+    //instance.counter.set(instance.counter.get() + 1);
+
+    var privateKey = CoinStack.ECKey.createKey();
+    $('#qrcode').qrcode({width: 256,height: 256,text: privateKey});
   },
 });
