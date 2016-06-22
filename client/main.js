739dfdcce551cd7116f752d473d3a6c39bfeecc1
KyungHoon <nephogram@gmail.com>
2016-06-22 18:35:10 +0900

    study-5

--- client/main.js
+++ client/main.js
@@ -1,28 +1,95 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+var Qrcodesvg = require("qrcodesvg");
+var BitcoinAddress = require("bitcoin-address");
 
 import './main.html';
 
-Wallets = new Meteor.Collection('wallets');
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-  Meteor.subscribe("wallets");
+UI.registerHelper('qrcode', function(id, text) {
+    if (text) {
+        $('#' + id).html(text);
+    }
 });
 
+Template.hello.onCreated(function helloOnCreated() {
+    Wallet = new Meteor.Collection('Wallet');
+    mywallet = Meteor.subscribe('mywallet');
+});
+
+Template.hello.rendered = function helloOnRendered() {
+    if (!this._rendered) {
+        this._rendered = true;
+        console.log('Template onLoad');
+        //$('#wallet_qrcode').html();
+    }
+};
+
 Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-  balance() {
-   return CoinStack.Math.toBitcoin( Wallets.findOne({}).balance );
-  }
+    wallet_address_qr() {
+        if (mywallet.ready()) {
+            var address = Wallet.findOne({})._id;
+            var qrcode = new Qrcodesvg("bitcoin:" + address, 200, {
+                "ecclevel": 2
+            });
+            var data = qrcode.generate({
+                "method": "classic",
+                "fill-colors": ["skyblue", "pink", "hotpink"]
+            }, {
+                "stroke-width": 1
+            });
+            return data;
+        }
+    },
+    wallet_address() {
+        if (mywallet.ready())
+            return Wallet.findOne({})._id;
+    },
+    wallet_history() {
+        if(mywallet.ready()) {
+            if(Session.get('wallet_history')) {
+                return Session.get('wallet_history');
+            }
+            var walletAddress = Wallet.findOne({})._id;
+            walletAddress = '15VScaUh8f9iP86y3GZGDgoh54dW5f7H1W';
+            Meteor.call('walletHistory', walletAddress, function(error, result) {
+                if(result) {
+                    console.log('result', result);
+                    Session.set('wallet_history', result);
+                }
+            });
+        }
+    }
 });
 
 Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+    'click #send' (event, instance) {
+        console.log('send');
+        console.log($('#send_address').val());
+        console.log($('#send_balance').val());
+
+        var send_address = $('#send_address').val();
+        var send_balance = $('#send_balance').val();
+
+        if(BitcoinAddress.validate(send_address)) {
+            console.log('pass');
+        } else {
+            alert('올바른 비트코인 주소를 입력해주세요.');
+            return;
+        }
+
+        if(isNaN(send_balance) || !(send_balance >= 0.0001)) {
+            alert('올바른 금액을 입력해주세요.');
+            return;
+        }
+
+        var privateKey = Wallet.findOne().privateKey;
+        console.log('privateKey', privateKey);
+    },
+    'click a[name=history]' (event, instance) {
+        console.log('clicked');
+        console.log(event.currentTarget.innerHTML);
+        console.log(instance);
+
+        
+    }
 });
--- server/main.js
+++ server/main.js
@@ -4,31 +4,57 @@ var accessKey = "80d1b821dcb89f965452de4ebbf7ae";
 var secretKey = "55e2b5ac1bb4a0486b477926efa8bc";
 client = new CoinStack(accessKey, secretKey);
 
-Wallets = new Meteor.Collection('wallets');
+Meteor.startup(function() {
+    // code to run on server at startup
+    Wallet = new Meteor.Collection('Wallet');
+    Exchange = new Meteor.Collection('Exchange');
+    var privateKey = CoinStack.ECKey.createKey();
+    var walletAddress = CoinStack.ECKey.deriveAddress(privateKey);
+    Wallet.insert({
+        _id: walletAddress,
+        privateKey: privateKey,
+        createAt: new Date()
+    });
 
-Meteor.publish("wallets", function () {
-  return Wallets.find({});
+    Meteor.setInterval(updateBTCPrice, 7000);
+    Meteor.setInterval(updateBTCBalance, 5000);
+
+    Meteor.publish('mywallet', function tasksPublication() {
+        return Wallet.find({}, { sort: { createAt: 1 }, limit: 1 });
+    });
 });
 
-Meteor.startup(() => {
-  // code to run on server at startup
-  Meteor.setInterval(function(){
-    console.log("hell ro world");
-
-    updateWallet();
-  }, 2000);
-});
-
-function updateWallet(){
-  var balance = client.getBalanceSync("12c6DSiU4Rq3P4ZxziKxzrL5LmMBrzjrJX");
-  console.log("balance: " + CoinStack.Math.toBitcoin(balance));
-
-  Wallets.upsert({
-    _id:"12c6DSiU4Rq3P4ZxziKxzrL5LmMBrzjrJX"
-  }, {
-    $set: {
-      _id:"12c6DSiU4Rq3P4ZxziKxzrL5LmMBrzjrJX",
-      balance:balance
+function updateBTCPrice() {
+    var url = 'https://api.bithumb.com/public/ticker';
+    var result = Meteor.http.get(url, {timeout:30000});
+    if(result.statusCode==200) {
+        var respJson = JSON.parse(result.content);
+        //console.log('response received: ', respJson.data);
+        Exchange.upsert('bithumb', respJson.data);
+    } else {
+        console.log('Response issue: ', result.statusCode);
+        var errorJson = JSON.parse(result.content);
+        throw new Meteor.Error(result.statusCode, errorJson.error);
     }
-  });
 }
+
+function updateBTCBalance() {
+    var walletAddress = Wallet.findOne({}, { sort: { createAt: 1 } })._id;
+    var walletBalance = client.getBalanceSync(walletAddress);
+    console.log(walletAddress);
+
+    Wallet.update(walletAddress, { '$set': {
+        balance: walletBalance
+    }});
+}
+
+Meteor.methods({
+    walletHistory: function(walletAddress) {
+        console.log('walletAddress', walletAddress);
+        var result = client.getTransactionsSync(walletAddress);
+        return result;
+    },
+    walletHistoryDetail: function(transactionId) {
+        console.log('walletHistoryDetail', transactionId);
+    }
+});
